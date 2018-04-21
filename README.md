52041168a7b218e061a00b9a90921256f0977e9e
jumasheff <jumasheff@gmail.com>
2018-04-21 12:18:31 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+Install dependencies:
+```
+sudo apt-get install libzbar-dev
+sudo apt-get install python3-dev
+sudo apt-get install python-dev
+```
+
+Create virtualenv with Python 2.7:
+```
+mkvirtualenv some_virtualenv -p python2
+```
+
+Activate virtualenv:
+```
+workon some_virtualenv
+```
+
+Install python packages from requirements.txt:
+```
+pip install -r requirements.txt
+```
---
+++ main.py
@@ -0,0 +1,28 @@
+import qrtools
+from alyn import Deskew
+
+# text_with_qr_30_deg.png is given for testing purposes.
+# use your image here
+input_file = 'text_with_qr_30_deg.png'
+output_file = 'text_with_qr_30_deg_deskewed.png'
+
+
+"""
+===================================================
+"""
+
+d = Deskew(
+    input_file=input_file,
+    display_image=False,
+    output_file=output_file,
+    r_angle=0
+)
+d.run()
+
+"""
+===================================================
+"""
+qr = qrtools.QR()
+qr.decode(output_file)
+
+print qr.data
